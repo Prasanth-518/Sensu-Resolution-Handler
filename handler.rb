97bfc2690d439c0ac56b44e51532f781a9731ee8
Prasanth-518 <prasanthvg3@gmail.com>
2021-04-26 20:22:36 +0530

    Updated handler

--- handler.rb
+++ handler.rb
@@ -11,19 +11,19 @@ status = event[:check][:status]
 entity_name = event[:entity][:metadata][:name]
 namespace = event[:entity][:metadata][:namespace]
 api_key = ARGV[0]
-url = "http://20.83.136.55:8080"
-res_check = event[:check][:labels]["resolution"]
+url = "http://20.72.151.214:8080"
+res_check = event[:check][:labels][:resolution]
 
 if status != 0
-    `curl -X POST \
-    -H "Authorization: Key #{api_key}" \
-    -H 'Content-Type: application/json' \
-    -d '{
-      "check": "#{res_check}",
-      "subscriptions": [
-        "entity:#{entity_name}"
-      ]
-    }' \
-    #{url}/api/core/v2/namespaces/#{namespace}/checks/#{res_check}/execute
-    `
+puts `curl -X POST \
+      -H "Authorization: Key #{api_key}" \
+      -H 'Content-Type: application/json' \
+      -d '{
+        "check": "#{res_check}",
+        "subscriptions": [
+          "entity:#{entity_name}"
+        ]
+      }' \
+      #{url}/api/core/v2/namespaces/#{namespace}/checks/#{res_check}/execute
+      `
 end
